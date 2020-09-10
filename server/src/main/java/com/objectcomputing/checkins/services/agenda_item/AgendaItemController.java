c389dd7c37c911d8a37fde840da0d72adcf39f60
Suman Maroju <sumanmaroju2020@u.northwestern.edu>
2020-09-10 03:58:26 -0500

    update user validation for agenda

--- server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemController.java
@@ -12,7 +12,6 @@ import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.annotation.Nullable;
-import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
@@ -21,25 +20,19 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import com.objectcomputing.checkins.services.memberprofile.MemberProfile;	
-import com.objectcomputing.checkins.services.memberprofile.currentuser.CurrentUserServicesImpl;	
-import com.objectcomputing.checkins.services.role.RoleType;
-import io.micronaut.http.HttpStatus;	
-import io.micronaut.security.utils.SecurityService;	
+import javax.inject.Singleton;
 
 @Controller("/services/agenda-item")
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "agenda-item")
+@Singleton
 public class AgendaItemController {
 
-    @Inject
-    AgendaItemServices agendaItemServices;
-
-    @Inject	
-    CurrentUserServicesImpl currentUserServices;	
-    @Inject	
-    SecurityService securityService;
+    private AgendaItemServices agendaItemServices;
+    public AgendaItemController(AgendaItemServices agendaItemServices) {
+        this.agendaItemServices = agendaItemServices;
+    }
 
     @Error(exception = AgendaItemBadArgException.class)
     public HttpResponse<?> handleBadArgs(HttpRequest<?> request, AgendaItemBadArgException e) {
@@ -50,169 +43,75 @@ public class AgendaItemController {
                 .body(error);
     }
 
-    // /**
-    //  * Create and save a new agenda item.
-    //  *
-    //  * @param agendaItem, {@link AgendaItemCreateDTO}
-    //  * @return {@link HttpResponse <AgendaItem>}
-    //  */
-    // @Post()
-    // public HttpResponse<AgendaItem> createAgendaItem(@Body @Valid AgendaItemCreateDTO agendaItem,
-    //                                                  HttpRequest<AgendaItemCreateDTO> request) {
-    //     AgendaItem newAgendaItem = agendaItemServices.save(new AgendaItem(agendaItem.getCheckinid(),
-    //             agendaItem.getCreatedbyid(), agendaItem.getDescription()));
-    //     return HttpResponse
-    //             .created(newAgendaItem)
-    //             .headers(headers -> headers.location(
-    //                     URI.create(String.format("%s/%s", request.getPath(), newAgendaItem.getId()))));
-    // }
-
-    /**	
-     * Save check-in details.	
-     * @param agendaItem	
-     * @return	
-     */	
-    @Post()	
-    public HttpResponse<AgendaItem> createCheckIn(@Body @Valid AgendaItemCreateDTO agendaItem, HttpRequest<AgendaItemCreateDTO> request) {	
-        MemberProfile currentUser = currentUserServices.currentUserDetails();	
-        Boolean isAdmin = securityService.hasRole(RoleType.Constants.ADMIN_ROLE);	
-        if(currentUser.getUuid().equals(agendaItem.getCreatedbyid()) || currentUser.getUuid().equals(agendaItem.getCheckinid()) || isAdmin) {	
-            AgendaItem newMemberCheckIn = agendaItemServices.save(new AgendaItem(agendaItem.getCheckinid(),agendaItem.getCreatedbyid(), agendaItem.getDescription()));	
-            return HttpResponse.created(newMemberCheckIn)	
-                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), newMemberCheckIn.getId()))));	
-        }	
-        return HttpResponse.status(HttpStatus.FORBIDDEN);	
+    /**
+     * Create and save a new agenda item.
+     *
+     * @param agendaItem, {@link AgendaItemCreateDTO}
+     * @return {@link HttpResponse <AgendaItem>}
+     */
+    @Post()
+    public HttpResponse<AgendaItem> createAgendaItem(@Body @Valid AgendaItemCreateDTO agendaItem,
+                                                     HttpRequest<AgendaItemCreateDTO> request) {
+        AgendaItem newAgendaItem = agendaItemServices.save(new AgendaItem(agendaItem.getCheckinid(),
+                agendaItem.getCreatedbyid(), agendaItem.getDescription()));
+        return HttpResponse
+                .created(newAgendaItem)
+                .headers(headers -> headers.location(
+                        URI.create(String.format("%s/%s", request.getPath(), newAgendaItem.getId()))));
     }
 
+    /**
+     * Update agenda item.
+     *
+     * @param agendaItem, {@link AgendaItem}
+     * @return {@link HttpResponse< AgendaItem >}
+     */
+    @Put()
+    public HttpResponse<?> updateAgendaItem(@Body @Valid AgendaItem agendaItem, HttpRequest<AgendaItem> request) {
+        AgendaItem updatedAgendaItem = agendaItemServices.update(agendaItem);
+        return HttpResponse
+                .ok()
+                .headers(headers -> headers.location(
+                        URI.create(String.format("%s/%s", request.getPath(), updatedAgendaItem.getId()))))
+                .body(updatedAgendaItem);
 
-    // /**
-    //  * Update agenda item.
-    //  *
-    //  * @param agendaItem, {@link AgendaItem}
-    //  * @return {@link HttpResponse< AgendaItem >}
-    //  */
-    // @Put()
-    // public HttpResponse<?> updateAgendaItem(@Body @Valid AgendaItem agendaItem, HttpRequest<AgendaItem> request) {
-    //     AgendaItem updatedAgendaItem = agendaItemServices.update(agendaItem);
-    //     return HttpResponse
-    //             .ok()
-    //             .headers(headers -> headers.location(
-    //                     URI.create(String.format("%s/%s", request.getPath(), updatedAgendaItem.getId()))))
-    //             .body(updatedAgendaItem);
-
-    // }
-
-	    /**	
-     * Update check in details	
-     * @param agendaItem	
-     * @return	
-     */	
-    @Put("/")	
-    public HttpResponse<?> update(@Body @Valid AgendaItem agendaItem, HttpRequest<AgendaItemCreateDTO> request) {	
-        MemberProfile currentUser = currentUserServices.currentUserDetails();	
-        Boolean isAdmin = securityService.hasRole(RoleType.Constants.ADMIN_ROLE);	
-        if(currentUser.getUuid().equals(agendaItem.getCreatedbyid()) || currentUser.getUuid().equals(agendaItem.getCheckinid()) || isAdmin) {	
-            AgendaItem updatedMemberCheckIn = agendaItemServices.update(agendaItem);	
-            return HttpResponse	
-                    .ok()	
-                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), updatedMemberCheckIn.getId()))))	
-                    .body(updatedMemberCheckIn);	
-        }	
-        return HttpResponse.status(HttpStatus.FORBIDDEN);	
     }
 
-
-
-    // /**
-    //  * Delete agenda item
-    //  *
-    //  * @param id, id of {@link AgendaItem} to delete
-    //  */
-    // @Delete("/{id}")
-    // public HttpResponse<?> deleteAgendaItem(UUID id) {
-    //     agendaItemServices.delete(id);
-    //     return HttpResponse
-    //             .ok();
-    // }
-
-    /**	
-     * 	
-     * @param id	
-     * @return	
-     */	
-    @Delete("/{id}")	
-    public AgendaItem deleteAgendaItem(@NotNull UUID id){	
-        MemberProfile currentUser = currentUserServices.currentUserDetails();	
-        Boolean isAdmin = securityService.hasRole(RoleType.Constants.ADMIN_ROLE);	
-        AgendaItem agendaItem = agendaItemServices.read(id);	
-        if(currentUser.getUuid().equals(agendaItem.getCreatedbyid()) || currentUser.getUuid().equals(agendaItem.getCheckinid()) || isAdmin) {	
-            return agendaItem;	
-        }	
-        return null;	
+    /**
+     * Delete agenda item
+     *
+     * @param id, id of {@link AgendaItem} to delete
+     */
+    @Delete("/{id}")
+    public HttpResponse<?> deleteAgendaItem(UUID id) {
+        agendaItemServices.delete(id);
+        return HttpResponse
+                .ok();
     }
 
-    // /**
-    //  * Get agenda item based off id
-    //  *
-    //  * @param id {@link UUID} of the agenda item entry
-    //  * @return {@link AgendaItem}
-    //  */
-    // @Get("/{id}")
-    // public AgendaItem readAgendaItem(UUID id) {
-    //     return agendaItemServices.read(id);
-    // }
-
-
-	/**	
-     * 	
-     * @param id	
-     * @return	
-     */	
-    @Get("/{id}")	
-    public AgendaItem readAgendaItem(@NotNull UUID id){	
-        MemberProfile currentUser = currentUserServices.currentUserDetails();	
-        Boolean isAdmin = securityService.hasRole(RoleType.Constants.ADMIN_ROLE);	
-        AgendaItem agendaItem = agendaItemServices.read(id);	
-        if(currentUser.getUuid().equals(agendaItem.getCreatedbyid()) || currentUser.getUuid().equals(agendaItem.getCheckinid()) || isAdmin) {	
-            return agendaItem;	
-        }	
-        return null;	
+    /**
+     * Get agenda item based off id
+     *
+     * @param id {@link UUID} of the agenda item entry
+     * @return {@link AgendaItem}
+     */
+    @Get("/{id}")
+    public AgendaItem readAgendaItem(UUID id) {
+        return agendaItemServices.read(id);
     }
 
-    // /**
-    //  * Find agenda items that match all filled in parameters, return all results when given no params
-    //  *
-    //  * @param checkinid   {@link UUID} of checkin
-    //  * @param createdbyid {@link UUID} of member
-    //  * @return {@link List < CheckIn > list of checkins}
-    //  */
-    // @Get("/{?checkinid,createdbyid}")
-    // public Set<AgendaItem> findAgendaItems(@Nullable UUID checkinid,
-    //                                        @Nullable UUID createdbyid) {
-    //     return agendaItemServices.findByFields(checkinid, createdbyid);
-    // }
-
-
-	// // /**	
-    // //  * Find AgendaItem details by checkinid or createdbyid. 	
-    // //  * @param checkinid	
-    // //  * @param createdbyid	
-    // //  * @return	
-    // //  */	
-    // @Get("/{?checkinid, createdbyid}")	
-    // public Set<AgendaItem> findByValue(@Nullable UUID checkinid, @Nullable UUID  createdbyid) {	
-    //     MemberProfile currentUser = currentUserServices.currentUserDetails();	
-    //     Boolean isAdmin = securityService.hasRole(RoleType.Constants.ADMIN_ROLE);	
-    //     // public Set<AgendaItem> findAgendaItems(@Nullable UUID checkinid, @Nullable UUID createdbyid);
-    //     Set<AgendaItem> agendaItemResult = agendaItemServices.findByFields(checkinid, createdbyid);	
-    //     if(isAdmin ||	
-    //             agendaItemResult.stream().allMatch(agendaItem -> agendaItem.getCreatedbyid().equals(currentUser.getUuid())) ||	
-    //             agendaItemResult.stream().anyMatch(agendaItem -> agendaItem.getCheckinid().equals(currentUser.getUuid()))) {	
-    //         return agendaItemResult;	
-    //     }	
-    //     return null;	
-    // }
-
+    /**
+     * Find agenda items that match all filled in parameters, return all results when given no params
+     *
+     * @param checkinid   {@link UUID} of checkin
+     * @param createdbyid {@link UUID} of member
+     * @return {@link List < CheckIn > list of checkins}
+     */
+    @Get("/{?checkinid,createdbyid}")
+    public Set<AgendaItem> findAgendaItems(@Nullable UUID checkinid,
+                                           @Nullable UUID createdbyid) {
+        return agendaItemServices.findByFields(checkinid, createdbyid);
+    }
 
     /**
      * Load agenda items
@@ -244,5 +143,7 @@ public class AgendaItemController {
                     .headers(headers -> headers.location(request.getUri()));
         }
     }
+
+
 }
 
--- server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemServicesImpl.java
@@ -3,82 +3,154 @@ package com.objectcomputing.checkins.services.agenda_item;
 import com.objectcomputing.checkins.services.checkins.CheckInRepository;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfileRepository;
 
-import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+import com.objectcomputing.checkins.services.memberprofile.MemberProfile;
+import com.objectcomputing.checkins.services.memberprofile.currentuser.CurrentUserServices;
+import com.objectcomputing.checkins.services.role.RoleType;
+import io.micronaut.security.utils.SecurityService;
+import javax.inject.Singleton;
+
+@Singleton
 public class AgendaItemServicesImpl implements AgendaItemServices {
 
-    @Inject
     private CheckInRepository checkinRepo;
-    @Inject
     private AgendaItemRepository agendaItemRepo;
-    @Inject
     private MemberProfileRepository memberRepo;
+    private SecurityService securityService;
+    private CurrentUserServices currentUserServices;
 
-    public AgendaItem save(AgendaItem agendaItem) {
-        AgendaItem agendaItemRet = null;
-        if (agendaItem != null) {
-            final UUID checkinId = agendaItem.getCheckinid();
-            final UUID createById = agendaItem.getCreatedbyid();
-            if (checkinId == null || createById == null) {
-                throw new AgendaItemBadArgException(String.format("Invalid agendaItem %s", agendaItem));
-            } else if (agendaItem.getId() != null) {
-                throw new AgendaItemBadArgException(String.format("Found unexpected id %s for agenda item", agendaItem.getId()));
-            } else if (!checkinRepo.findById(checkinId).isPresent()) {
-                throw new AgendaItemBadArgException(String.format("CheckIn %s doesn't exist", checkinId));
-            } else if (!memberRepo.findById(createById).isPresent()) {
-                throw new AgendaItemBadArgException(String.format("Member %s doesn't exist", createById));
-            }
-
-            agendaItemRet = agendaItemRepo.save(agendaItem);
-        }
-        return agendaItemRet;
+    public AgendaItemServicesImpl(AgendaItemRepository agendaItemRepo, CheckInRepository checkinRepo, MemberProfileRepository memberRepo, SecurityService securityService, CurrentUserServices currentUserServices) {
+        this.agendaItemRepo = agendaItemRepo;
+        this.checkinRepo = checkinRepo;
+        this.memberRepo = memberRepo;
+        this.securityService = securityService;
+        this.currentUserServices = currentUserServices;
     }
 
+    @Override
+    public AgendaItem save(@NotNull AgendaItem agendaItem) {
+
+        final UUID createById = agendaItem.getCreatedbyid();
+        final UUID checkinId = agendaItem.getCheckinid();
+
+        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+
+        if(agendaItem.getId()!=null) {
+            throw new AgendaItemBadArgException(String.format("Found unexpected id for agendaItem  %s", agendaItem.getId()));
+        } else if (checkinId == null || createById == null) {
+            throw new AgendaItemBadArgException(String.format("Invalid agendaItem %s", agendaItem));
+        } else if(memberRepo.findById(createById).isEmpty()) {
+            throw new AgendaItemBadArgException(String.format("Member %s doesn't exist", createById));
+        } else if (!checkinRepo.findById(checkinId).isPresent()) {
+            throw new AgendaItemBadArgException(String.format("CheckIn %s doesn't exist", checkinId));
+        } else if(!isAdmin) {
+            if(!currentUser.getId().equals(agendaItem.getCreatedbyid())) {
+                throw new AgendaItemBadArgException(String.format("Member %s is unauthorized to do this operation", currentUser.getId()));
+            }
+        }
+
+        return agendaItemRepo.save(agendaItem);
+    }
+
+    @Override
     public AgendaItem read(@NotNull UUID id) {
-        return agendaItemRepo.findById(id).orElse(null);
 
-    }
+        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
 
-    public AgendaItem update(AgendaItem agendaItem) {
-        AgendaItem agendaItemRet = null;
-        if (agendaItem != null) {
-            final UUID id = agendaItem.getId();
-            final UUID checkinId = agendaItem.getCheckinid();
-            final UUID createById = agendaItem.getCreatedbyid();
-            if (checkinId == null || createById == null) {
-                throw new AgendaItemBadArgException(String.format("Invalid agendaItem %s", agendaItem));
-            } else if (id == null || !agendaItemRepo.findById(id).isPresent()) {
-                throw new AgendaItemBadArgException(String.format("Unable to locate agendaItem to update with id %s", id));
-            } else if (!checkinRepo.findById(checkinId).isPresent()) {
-                throw new AgendaItemBadArgException(String.format("CheckIn %s doesn't exist", checkinId));
-            } else if (!memberRepo.findById(createById).isPresent()) {
-                throw new AgendaItemBadArgException(String.format("Member %s doesn't exist", createById));
+        AgendaItem result = agendaItemRepo.findById(id).orElse(null);
+
+        if (result == null) {
+            throw new AgendaItemBadArgException(String.format("Invalid agendaItem id %s", id));
+        } else if(!isAdmin) {
+            if(!currentUser.getId().equals(result.getCreatedbyid())) {
+                throw new AgendaItemBadArgException(String.format("Member %s is unauthorized to do this operation", currentUser.getId()));
             }
-
-            agendaItemRet = agendaItemRepo.update(agendaItem);
         }
-        return agendaItemRet;
+
+        return result;
     }
 
+    @Override
+    public AgendaItem update(@NotNull AgendaItem agendaItem) {
+
+        final UUID id = agendaItem.getId();
+        final UUID createById = agendaItem.getCreatedbyid();
+        final UUID checkinId = agendaItem.getCheckinid();
+        // LocalDateTime chkInDate = checkIn.getCheckInDate();
+
+        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+
+        if(id==null||!agendaItemRepo.findById(id).isPresent()) {
+            throw new AgendaItemBadArgException(String.format("Unable to find agendaItem record with id %s", agendaItem.getId()));
+        } else if(checkinId == null || createById == null) {
+            throw new AgendaItemBadArgException(String.format("Invalid agendaItem %s", agendaItem));
+        } else if(!memberRepo.findById(createById).isPresent()) {
+            throw new AgendaItemBadArgException(String.format("Member %s doesn't exist", createById));
+        } else if (!checkinRepo.findById(checkinId).isPresent()) {
+            throw new AgendaItemBadArgException(String.format("CheckIn %s doesn't exist", checkinId));
+        } else if(!isAdmin) {
+            if(!currentUser.getId().equals(agendaItem.getCreatedbyid())) {
+                throw new AgendaItemBadArgException(String.format("Member %s is unauthorized to do this operation", currentUser.getId()));
+            // } else if(checkinRepo.findById(id).get().isCompleted()) {
+            //     throw new AgendaItemBadArgException(String.format("Checkin with id %s is complete and cannot be updated", agendaItem.getId()));
+            }
+        }
+
+        return agendaItemRepo.update(agendaItem);
+    }
+
+    @Override
     public Set<AgendaItem> findByFields(UUID checkinid, UUID createdbyid) {
         Set<AgendaItem> agendaItems = new HashSet<>();
-        agendaItemRepo.findAll().forEach(agendaItems::add);
+        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
 
-        if (checkinid != null) {
-            agendaItems.retainAll(agendaItemRepo.findByCheckinid(checkinid));
-        }
-        if (createdbyid != null) {
-            agendaItems.retainAll(agendaItemRepo.findByCreatedbyid(createdbyid));
+        if(isAdmin || currentUser.getId().equals(createdbyid) ||
+                currentUser.getId().equals(memberRepo.findById(createdbyid).get().getPdlId())) {
+
+            agendaItemRepo.findAll().forEach(agendaItems::add);
+            if (createdbyid != null) {
+                agendaItems.retainAll(agendaItemRepo.findByCheckinid(checkinid));
+            } else if (checkinid != null) {
+                agendaItems.retainAll(agendaItemRepo.findByCreatedbyid(createdbyid));
+            // } else if (completed != null) {
+            //     agendaItems.retainAll(checkinRepo.findByCompleted(completed));
+            }
+            return agendaItems;
         }
 
-        return agendaItems;
+        throw new AgendaItemBadArgException(String.format("Member %s is unauthorized to do this operation", currentUser.getId()));
     }
 
     public void delete(@NotNull UUID id) {
+        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+
+        AgendaItem result = agendaItemRepo.findById(id).orElse(null);
+
+        if (result == null) {
+            throw new AgendaItemBadArgException(String.format("Invalid agendaItem id %s", id));
+        } else if(!isAdmin) {
+            if(!currentUser.getId().equals(result.getCreatedbyid())) {
+                throw new AgendaItemBadArgException(String.format("Member %s is unauthorized to do this operation", currentUser.getId()));
+            }
         agendaItemRepo.deleteById(id);
+        }
+
     }
+
 }
+
+
