53f4bb62250d6ebe84ca7d579f0b3cb2bb9b0758
Pramukh Bagur <bagurp@OCI1852.attlocal.net>
2020-10-15 17:45:19 -0500

    feature #385: add Google accessor and Authenticator for Google Drive API

--- server/src/main/java/com/objectcomputing/checkins/util/googleApiAccess/GoogleAccessor.java
+++ server/src/main/java/com/objectcomputing/checkins/util/googleApiAccess/GoogleAccessor.java
@@ -5,6 +5,7 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.drive.Drive;
 import com.google.auth.http.HttpCredentialsAdapter;
 import io.micronaut.context.annotation.Property;
@@ -35,9 +36,22 @@ public class GoogleAccessor {
      * @throws IOException
      */
     public Drive accessGoogleDrive() throws IOException {
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(authenticator.setupCredentials());
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(authenticator.setupCredentialsForDrive());
         return new Drive
                 .Builder(httpTransport, JSON_FACTORY, requestInitializer)
+                .setApplicationName(applicationName)
+                .build();
+    }
+
+    /**
+     * Create and return the google directory access object
+     *
+     * @return a google directory access object
+     * @throws IOException
+     */
+    public Directory accessGoogleDirectory() throws IOException {
+        return new Directory
+                .Builder(httpTransport, JSON_FACTORY, authenticator.setupCredentialsForDirectory(httpTransport, JSON_FACTORY))
                 .setApplicationName(applicationName)
                 .build();
     }
--- server/src/main/java/com/objectcomputing/checkins/util/googleApiAccess/GoogleAuthenticator.java
+++ server/src/main/java/com/objectcomputing/checkins/util/googleApiAccess/GoogleAuthenticator.java
@@ -1,5 +1,14 @@
 package com.objectcomputing.checkins.util.googleApiAccess;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.admin.directory.DirectoryScopes;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.objectcomputing.checkins.security.GoogleServiceConfiguration;
 import io.micronaut.context.annotation.Property;
@@ -8,6 +17,8 @@ import javax.inject.Singleton;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Singleton
 public class GoogleAuthenticator {
@@ -33,7 +44,7 @@ public class GoogleAuthenticator {
      * @return An authorized Credential object.
      * @throws IOException If the configured file cannot be found.
      */
-    GoogleCredentials setupCredentials() throws IOException {
+    GoogleCredentials setupCredentialsForDrive() throws IOException {
 
         InputStream in = new ByteArrayInputStream(gServiceConfig.toString().getBytes(StandardCharsets.UTF_8));
         GoogleCredentials credentials = GoogleCredentials.fromStream(in);
@@ -44,6 +55,28 @@ public class GoogleAuthenticator {
         }
 
         return scopes.isEmpty() ? credentials : credentials.createScoped(scopes);
+    }
+
+    Credential setupCredentialsForDirectory(final NetHttpTransport HTTP_TRANSPORT, final JsonFactory JSON_FACTORY) throws IOException {
+
+        String TOKENS_DIRECTORY_PATH = "tokens";
+        List<String> SCOPES = Collections.singletonList(DirectoryScopes.ADMIN_DIRECTORY_USER_READONLY);
+
+        // Load client secrets.
+        InputStream in = new ByteArrayInputStream(gServiceConfig.toString().getBytes(StandardCharsets.UTF_8));
+        if (in == null) {
+            throw new FileNotFoundException("Credentials not found while using Google default credentials");
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
 
